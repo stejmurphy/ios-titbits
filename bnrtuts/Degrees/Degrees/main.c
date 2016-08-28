c5c0b5c3d152cd9c143db9964c050e782610896f
Stephen Murphy <stephen.jd.murphy@gmail.com>
2016-08-28 14:02:10 +0100

    Cel to Far

--- bnrtuts/Degrees/Degrees/main.c
+++ bnrtuts/Degrees/Degrees/main.c
@@ -7,9 +7,26 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+float lastTemp;
+
+float fahrenheitFromCelsius(float cel){
+    
+    lastTemp = cel;
+    float fahr = cel * 1.8 + 32.0;
+    
+    printf("%f Celsius is %f Fahrenheit.\n", cel, fahr);
+    return fahr;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+  
+    float freezeInC = 0;
+    float freezeinF = fahrenheitFromCelsius(freezeInC);
+    
+    printf("Water freezes at %f degrees Fahrenheit.\n", freezeinF);
+    printf("The last temperture converted was %f.\n", lastTemp);
+
+    return EXIT_SUCCESS;
 }
